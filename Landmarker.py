e23b5c60346356b2ae8e9b6bd50780595cc8d23a
RohitRoby <rohitroby12@gmail.com>
2024-09-12 23:34:07 +0100

    need to do changes to add the landmarks on to the live s=video stream

--- Landmarker.py
+++ Landmarker.py
@@ -1,8 +1,11 @@
 # STEP 1: Import the necessary modules.
 import cv2
+import numpy as np
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
 
 # STEP 2: Create an PoseLandmarker object.
 model_path = 'pose_landmarker_heavy.task'
@@ -16,19 +19,37 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 # def print_result(result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
 #     print('pose landmarker result: {}'.format(result))
 
-def draw_landmarks_on_livestream(image, ann_img):
-    if ann_img is None:
-        return image    
-    mp_drawing = mp.solutions.drawing_utils
-    mp_pose = mp.solutions.pose
-    mp_drawing_style = mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2)
-    mp_drawing.draw_landmarks(image, ann_img.pose_landmarks, mp_pose.POSE_CONNECTIONS, mp_drawing_style)
-    # cv2.imshow('frame', image)
-    return image
+def process_result(frame, landmarks: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int): # type: ignore
+    # ann_frame = draw_landmarks_on_image(frame, landmarks)
+    return frame
+    # cv2.imshow('frame', ann_frame)
+    # if cv2.waitKey(1) == ord('q'):
+    #     return
+    
+def draw_landmarks_on_image(rgb_image, detection_result):
+  pose_landmarks_list = detection_result.pose_landmarks
+  annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected poses to visualize.
+  for idx in range(len(pose_landmarks_list)):
+    pose_landmarks = pose_landmarks_list[idx]
+
+    # Draw the pose landmarks.
+    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    pose_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+    ])
+    solutions.drawing_utils.draw_landmarks(
+      annotated_image,
+      pose_landmarks_proto,
+      solutions.pose.POSE_CONNECTIONS,
+      solutions.drawing_styles.get_default_pose_landmarks_style())
+  return annotated_image
 
 options = PoseLandmarkerOptions(
     base_options=BaseOptions(model_asset_path=model_path),
-    running_mode=VisionRunningMode.LIVE_STREAM,result_callback=draw_landmarks_on_livestream)
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=process_result)
 
 with PoseLandmarker.create_from_options(options) as landmarker:
   # The landmarker is initialized. Use it here.
@@ -40,22 +61,21 @@ with PoseLandmarker.create_from_options(options) as landmarker:
     while True:
         #capture Frame by frame
         ret, frame = cap.read()
-        
-        
         #if frame is read correctly ret is True
         if not ret:
             print("can't receive frame (stream end?). Exiting ...")
             break
-        #our preperation on the frame come here
+        
         
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+        # Run the PoseLandmarker on the image.
         landmarks = landmarker.detect_async(mp_image, int(cap.get(cv2.CAP_PROP_POS_MSEC)))
         # print(ann_img)
-        ann_frame = draw_landmarks_on_livestream(frame, landmarks)
+        ann_frame = draw_landmarks_on_image(frame, landmarks)
         cv2.imshow('frame', ann_frame)
-        
         if cv2.waitKey(1) == ord('q'):
             break
+        
     cap.release()
     cv2.destroyAllWindows()
     # cv2.imshow('frame', annotated_image)
